a2afc03f01b01318acc3cc32176fa7b6eedb45cd
mavlonbek-sirojiddinov <mavlonbeksirojiddinov33@gmail.com>
2023-02-15 14:24:12 +0500

    add sections

---
+++ js/data.js
@@ -0,0 +1,22 @@
+const data = {
+  nav: [
+    {
+      id: 1,
+      titleUZ: 'Kompaniya haqida',
+      titleEN: 'About the componiy',
+      titleRU: 'About the componiy',
+    },
+    {
+      id: 2,
+      titleUZ: 'Faktlar',
+      titleEN: 'The Facts',
+      titleRU: 'About the componiy',
+    },
+    {
+      id: 2,
+      titleUZ: 'Mahsulot',
+      titleEN: 'The Facts',
+      titleRU: 'About the componiy',
+    }
+  ]
+}
